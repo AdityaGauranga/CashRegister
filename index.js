371b6ea5d7abb2ff8c754a8ea3e393de6f056dfb
AdityaGauranga <aaditya.sarma108@gmail.com>
2021-12-09 12:59:22 +0530

    input buttons logic

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("cash-given");
 const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
 
+checkButton.addEventListener("click", function validateBillnCashAmount(){
+    message.style.display = "none";
+    if (billAmount.value > 0){
+
+    }
+    else {
+        message.style.display = "block";
+        message.innerText = "The bill amount should be greater than 0";
+    } 
+});
