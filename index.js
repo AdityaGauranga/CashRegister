8808f292416085eb30ac155fbcd2d3e414dd1de4
AdityaGauranga <aaditya.sarma108@gmail.com>
2021-12-09 14:08:49 +0530

    calculation part of the logic

--- index.js
+++ index.js
@@ -2,34 +2,45 @@ const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelector(".no-of-notes");
+const availableNotes = [2000, 500, 100, 20, 10, 1];
 
-checkButton.addEventListener("click", function validateBillnCashAmount(){
+checkButton.addEventListener("click", function validateBillnCashAmount() {
     hideMessage();
-    if (billAmount.value > 0){
-        if(cashGiven.value >= billAmount.value ) {
+    if (billAmount.value > 0) {
+        if (cashGiven.value >= billAmount.value) {
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
-        }
-        else {
+        } else {
             showMessage(
                 "The cash provided should atleast be equal to the bill amount"
             );
 
         }
 
-    }
-    else {
+    } else {
         showMessage("Invalid Bill Amount");
-    } 
+    }
 });
 
-fucntion calculateChange(){
-    
+function calculateChange() {
+    // go over all the available notes
+    for (let i = 0; i < availableNotes.length; i++) {
+        // no of notes need for the denomination
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        
+        // amount left after calculating the number of notes needed
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        
+        //updating the no of notes in the table for the current amount
+        noOfNotes[i].innerText = numberOfNotes;
+    }
 }
 
 function hideMessage() {
     message.style.display = "none";
 }
+
 function showMessage(msg) {
     message.style.display = "block";
     message.innerText = msg;
